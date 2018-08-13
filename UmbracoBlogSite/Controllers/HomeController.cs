5e986572eb26b2517b88e55cb29f8c73f7546539
Adulis <adulis@bredband2.com>
2018-08-13 16:25:07 +0200

    .

--- UmbracoBlogSite/Controllers/HomeController.cs
+++ UmbracoBlogSite/Controllers/HomeController.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web;
 using Umbraco.Web.Mvc;
+using UmbracoBlogSite.Models;
+using Archetype.Models;
 
 namespace UmbracoBlogSite.Controllers
 {
@@ -12,7 +16,22 @@ namespace UmbracoBlogSite.Controllers
         private const string PARTIA_VIEW_FOLDER = "/Views/Partials/Home/";
         public ActionResult RenderFeatured()
         {
-            return PartialView(PARTIA_VIEW_FOLDER+"_Featured.cshtml");
+            List<FeaturedItem> model = new List<FeaturedItem>();
+            IPublishedContent homePage = CurrentPage.AncestorOrSelf(1).DescendantsOrSelf().Where(x => x.DocumentTypeAlias == "home").FirstOrDefault();
+            ArchetypeModel featuredItems = homePage.GetPropertyValue<ArchetypeModel>("featuredItems");
+
+            foreach (ArchetypeFieldsetModel fieldSet in featuredItems)
+            {
+                int imageId = fieldSet.GetValue<int>("image");
+                var mediaItem = Umbraco.Media(imageId);
+                string imageUrl = mediaItem.Url;
+
+                int pageId = fieldSet.GetValue<int>("page");
+                IPublishedContent linkedToPage = Umbraco.TypedContent(pageId);
+                string linkUrl = linkedToPage.Url;
+                model.Add(new FeaturedItem(fieldSet.GetValue<string>("name"),fieldSet.GetValue<string>("category"), imageUrl, linkUrl));
+            }
+            return PartialView(PARTIA_VIEW_FOLDER+"_Featured.cshtml",model);
         }
 
         
