ec62b14743d861a3c31c6318745e3e1eacc09c4e
Adulis <adulis@bredband2.com>
2018-08-13 13:32:00 +0200

    Initial commit

---
+++ UmbracoBlogSite/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoBlogSite.Controllers
+{
+    public class HomeController : SurfaceController
+    {
+        private const string PARTIA_VIEW_FOLDER = "/Views/Partials/Home/";
+        public ActionResult RenderFeatured()
+        {
+            return PartialView(PARTIA_VIEW_FOLDER+"_Featured.cshtml");
+        }
+
+        
+        public ActionResult RenderServices()
+        {
+            return PartialView(PARTIA_VIEW_FOLDER + "_Services.cshtml");
+        }
+      
+        public ActionResult RenderBlog()
+        {
+            return PartialView(PARTIA_VIEW_FOLDER + "_Blog.cshtml");
+        }
+        public ActionResult RenderClients()
+        {
+            return PartialView(PARTIA_VIEW_FOLDER + "_Clients.cshtml");
+        }
+    }
+}
---
+++ UmbracoBlogSite/config/Archetype.config.js
@@ -0,0 +1,4 @@
+{
+  "Id": "403fba3f-ada7-4e38-b44b-adcac43f523d",
+  "CheckForUpdates": true
+}
